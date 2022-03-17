57f00bcd10ae582da33b5246633f36afa047c7e7
Melon <56051776+melonboy10@users.noreply.github.com>
2022-03-16 21:13:18 -0700

    Add extra files

--- src/main/java/me/melonboy10/midi2building/Main.java
+++ src/main/java/me/melonboy10/midi2building/Main.java
@@ -1,21 +1,22 @@
 package me.melonboy10.midi2building;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
-    File datapackOutput;
-    File somg = new File("src/main/resources/defaultSongs/11.mid");
-    Scanner reader;
+    static File datapackOutput;
+    static File song = new File("src/main/resources/defaultSongs/11.mid");
+    static Scanner reader;
     {
         reader = new Scanner(String.valueOf(this.getClass().getResource("src/main/resources/resourceLocations.txt")));
         datapackOutput = new File(reader.nextLine());
     }
 
     public static void main(String[] args) {
+        MidiParser parser = new MidiParser(song);
+        final HashMap<String, SoundAtlas> noteToSoundType = parser.getNoteToSoundType();
 
     }
 
---
+++ src/main/java/me/melonboy10/midi2building/MidiParser.java
@@ -0,0 +1,61 @@
+package me.melonboy10.midi2building;
+
+import me.melonboy10.midi2building.SoundAtlas;
+
+import javax.sound.midi.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MidiParser {
+    private static final int NOTE_ON = 0x90;
+    private static final int NOTE_OFF = 0x80;
+    private static final String[] NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+
+    private File midiFile;
+    private Sequence sequence;
+    private final HashMap<String, Integer> noteCount = new HashMap<>();
+    private final HashMap<String, SoundAtlas> noteToSoundType = new HashMap<>();
+    private final ArrayList<Long> notes = new ArrayList<>();
+
+    public MidiParser(File midiFile) {
+        this.midiFile = midiFile;
+        try {
+            sequence = MidiSystem.getSequence(this.midiFile);
+        } catch (Exception e) {
+            System.out.println("Failed to read midi file!");
+        }
+
+        for (Track track :  sequence.getTracks()) {
+            for (int i = 0; i < track.size(); i++) {
+                MidiEvent event = track.get(i);
+                MidiMessage message = event.getMessage();
+                if (message instanceof ShortMessage sm) {
+//                    System.out.print("Channel: " + sm.getChannel() + " ");
+                    if (sm.getCommand() == NOTE_ON) {
+                        notes.add(event.getTick());
+                        int key = sm.getData1();
+                        int octave = (key / 12)-1;
+                        int note = key % 12;
+                        String noteName = octave + NOTE_NAMES[note];
+
+                        if (noteCount.containsKey(noteName)) {
+                            noteCount.put(noteName, noteCount.get(noteName) + 1);
+                        } else {
+                            noteCount.put(noteName,1);
+                            noteToSoundType.put(noteName, SoundAtlas.NULL);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public HashMap<String, Integer> getNoteCount() {
+        return noteCount;
+    }
+    public HashMap<String, SoundAtlas> getNoteToSoundType() {
+        return noteToSoundType;
+    }
+    public ArrayList<Long> getNotes() { return notes; }
+}
